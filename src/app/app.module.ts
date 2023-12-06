8dd823593180ec1e3ce7f497d48c955a709ca945
gabriel <araujo_gabriel@id.uff.br>
2023-12-06 19:03:24 -0300

    fixing app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -11,8 +11,8 @@ import { SharedModule } from './shared/shared.module';
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    SharedModule,
     AppRoutingModule
   ],
   providers: [],
